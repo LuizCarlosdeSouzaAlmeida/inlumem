1086d6d1468b478a0ca68b07399d405120e3a96b
Rafael Rosa <rafaelxsantosx@hotmail.com>
2023-05-14 22:22:11 -0300

    Altera orientação do sprite conforme movimento do jogador

--- Assets/MainCharacterScript.cs
+++ Assets/MainCharacterScript.cs
@@ -1,10 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MainCharacterScript : MonoBehaviour
 {
 	public Rigidbody2D mainCharacterRb;
+	public SpriteRenderer spriteRenderer;
+
 	public float jumpSpeed;
 	public float movementSpeed;
 	private bool IsJumpRequested
@@ -12,6 +12,15 @@ public class MainCharacterScript : MonoBehaviour
 		get =>
 			// Input.GetAxisRaw("Vertical") > 0 ||
 			Input.GetButtonDown("Jump");
+	}
+
+	private bool CharacterTurnedDirection
+	{
+		get
+		{
+			float movement = Input.GetAxis("Horizontal");
+			return (movement < 0 && !spriteRenderer.flipX) || (movement > 0 && spriteRenderer.flipX);
+		}
 	}
 
 	void Start()
@@ -28,6 +37,11 @@ public class MainCharacterScript : MonoBehaviour
 	void HandleMovement()
 	{
 		float movement = Input.GetAxis("Horizontal") * movementSpeed;
+
+		if (CharacterTurnedDirection)
+		{
+			spriteRenderer.flipX = !spriteRenderer.flipX;
+		}
 
 		transform.Translate(movement, 0, 0);
 	}
