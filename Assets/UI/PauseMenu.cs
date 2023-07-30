a5ce2b0cf0bb6166b1018affc16da88ffeea7810
Luciano Vianna <luciano11v11@gmail.com>
2023-07-30 16:56:27 -0300

    add efeitos sonoros e fix bugs

--- Assets/UI/PauseMenu.cs
+++ Assets/UI/PauseMenu.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,11 +9,21 @@ public class PauseMenu : MonoBehaviour
     public static bool GameIsPaused = false;
 
     public GameObject pauseMenuUI;
+    private AudioSource AudioSource;
+	[SerializeField] private AudioClip MenuHoverSound;
+	[SerializeField] private AudioClip MenuClickSound;
+
+    void Awake()
+	{
+		AudioSource = GetComponent<AudioSource>();
+	}
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
+            AudioSource.PlayOneShot(MenuClickSound);
+
             if (GameIsPaused) {
                 Resume();
             } else {
@@ -23,8 +34,11 @@ public class PauseMenu : MonoBehaviour
 
     public void Resume()
     {
+        AudioSource.PlayOneShot(MenuClickSound);
+
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
+
         GameIsPaused = false;
     }
     
@@ -32,24 +46,55 @@ public class PauseMenu : MonoBehaviour
     {
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
+
         GameIsPaused = true;
     }
 
     public void LoadMainMenu()
     {
-        Time.timeScale = 1f;
-        Loader.Load(Loader.Scene.MainMenuScene);
+        AudioSource.PlayOneShot(MenuClickSound);
+
+        StartCoroutine(WaitForAudioToLoad(() => {
+            GameIsPaused = false;
+            
+            Time.timeScale = 1f;
+            Loader.Load(Loader.Scene.MainMenuScene);
+        }));
     }
 
     public void RestartGame()
     {
-        Time.timeScale = 1f;
-        Loader.Load(Loader.Scene.SampleScene);
+        AudioSource.PlayOneShot(MenuClickSound);
+        
+        StartCoroutine(WaitForAudioToLoad(() => {
+            GameIsPaused = false;
+            
+            Time.timeScale = 1f;
+            Loader.Load(Loader.Scene.SampleScene);
+        }));
     }
 
     public void QuitGame()
     {
-        Application.Quit();
+        AudioSource.PlayOneShot(MenuClickSound);
+        
+        StartCoroutine(WaitForAudioToLoad(() => {
+            GameIsPaused = false;
+            
+            Application.Quit();
+        }));
+    }
+
+    private IEnumerator WaitForAudioToLoad(Action afterWait)
+	{
+		yield return new WaitWhile(() => AudioSource.isPlaying);
+		
+        afterWait?.Invoke();
+	}
+
+    public void triggerSoundOnHover()
+    {
+        AudioSource.PlayOneShot(MenuHoverSound);
     }
 }
 
